8e54b77565e57d58813245d4dac2d975ab7b2d96
Krish Choudhary <kdhakla333@gmail.com>
2024-02-11 21:14:27 +0530

    bottom navigation bar

--- lib/screens/library_home.dart
+++ lib/screens/library_home.dart
@@ -1,13 +1,37 @@
 import 'package:flutter/material.dart';
 
-class LibraryHome extends StatelessWidget {
+class LibraryHome extends StatefulWidget {
   const LibraryHome({super.key});
 
+  @override
+  State<LibraryHome> createState() => _LibraryHomeState();
+}
+
+class _LibraryHomeState extends State<LibraryHome> {
+  var selectedPageIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Home')),
       body: const Text("Home page"),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (index) {
+          setState(() {
+            selectedPageIndex = index;
+          });
+        },
+        currentIndex: selectedPageIndex,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.menu_book_rounded),
+            label: 'ebooks',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.contact_page_outlined),
+            label: 'Contact',
+          )
+        ],
+      ),
     );
   }
 }
