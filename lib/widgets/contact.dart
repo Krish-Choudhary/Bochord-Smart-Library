efbe7119200c140e6d7aa4f405de2ae68a21be79
Krish Choudhary <kdhakla333@gmail.com>
2024-02-27 18:52:03 +0530

    Contact Screen

--- lib/widgets/contact.dart
+++ lib/widgets/contact.dart
@@ -5,6 +5,16 @@ class Contact extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Contact Us",
+          style: TextStyle(
+            color: Theme.of(context).colorScheme.onPrimary,
+            fontWeight: FontWeight.normal,
+          ),
+        ),
+        backgroundColor: Theme.of(context).colorScheme.primary,
+      ),
       body: ListView(
         children: const [
           SizedBox(height: 20),
@@ -14,6 +24,22 @@ class Contact extends StatelessWidget {
               backgroundImage: NetworkImage(
                   'https://iiitu.ac.in/wp-content/uploads/2022/01/mr_ankushkapoor.jpg'),
             ),
+          ),
+          // SizedBox(height: 10),
+          // Center(child: Text("Mr. Ankush Kapoor",style: TextStyle(fontSize: 18),)),
+          ListTile(
+            title: Text('Name: Mr. Ankush Kapoor'),
+          ),
+          ListTile(
+            title: Text('Designation: Librarian'),
+          ),
+          ListTile(
+            leading: Icon(Icons.phone),
+            title: Text('+919418953816'),
+          ),
+          ListTile(
+            leading: Icon(Icons.email),
+            title: Text('ankush@iiitu.ac.in'),
           ),
         ],
       ),
