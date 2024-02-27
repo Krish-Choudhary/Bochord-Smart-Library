2b84558c06d14d8c4df9506f5ee8cb9cca7e6262
Krish Choudhary <kdhakla333@gmail.com>
2024-02-27 22:30:52 +0530

    shifting contact.dart file

--- lib/screens/library_home.dart
+++ lib/screens/library_home.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:library_app/widgets/contact.dart';
+import 'package:library_app/screens/contact.dart';
 
 final _firebase = FirebaseAuth.instance;
 
